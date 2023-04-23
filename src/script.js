7b96f46fd039bdb0423cd8f2ac5e8e6450c99114
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-04-23 21:46:49 +0300

    new information 23.04.23

--- src/script.js
+++ src/script.js
@@ -56,14 +56,14 @@ const myTable = {
             title: 'CASE POWERCASE',
             isProcessor: true,
             processor: 'AMD Ryzen 7 5700G with Radeon Graphics',
-            processorPower: 4.30(GHz),
+            processorPower: 4.30,
             isVideoCard: false,
             videoCard: '',
             iSOperativeMemory: true,
             nameOZU: 'Fury 16GB',
-            OperativeMemory: 16.0(GB),
+            OperativeMemory: 16.0,
             typeSistem: 'operating sistem',
-            bitSistem: 64(Bit),
+            bitSistem: 64,
             isMotherboard: true,
             Motherboard: 'TUF Gaming Pro 2',
             isPowerUnit: true,
@@ -86,7 +86,7 @@ const myTable = {
         },
         organizer: {
             color: 'white',
-            size: 60(mm),
+            size: 60,
             things: [
             {
                 id: 1,
@@ -131,3 +131,28 @@ const myTable = {
 // console.log(object.technologies_obj[6].title)
 
 
+// const nearestSq = (n) => {
+//     let root = Math.floor(Math.sqrt(n));
+//     let lowerSq = root ** 2
+//     let upperSq = (root + 1) ** 2
+//     if(root ** 2 === n) return n;
+//     else {
+//         if(lowerSq <= upperSq){
+//             return lowerSq;
+//         } 
+//         else return upperSq
+//     } 
+// }
+
+// console.log(nearestSq(1))
+// console.log(nearestSq(3))
+// console.log(nearestSq(4))
+// console.log(nearestSq(5))
+// console.log(nearestSq(7))
+// console.log(nearestSq(8))
+// console.log(nearestSq(9))
+
+// let birthday = new Date(2020, 11, 20);
+// let copy = new Date();
+// // console.log(copy.getTime())
+// console.log(birthday)
