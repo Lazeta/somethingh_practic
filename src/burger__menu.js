8121889b99560204a327afb3283608a8ca45f0a6
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-05-07 02:51:50 +0300

    add fun closing burger menu if press 'Escape' and 'Esc'

--- src/burger__menu.js
+++ src/burger__menu.js
@@ -6,7 +6,15 @@
 
 // для бургер меню из 4 полос и более
 const burgerMenu = document.querySelector('.burger-menu');
+const burgerIcon = document.querySelector('.nav');
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active');
     document.querySelector('.nav').classList.toggle('open');
 });
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Escape' || e.key === 'Esc'){
+        burgerMenu.classList.remove('active');
+        burgerIcon.classList.remove('open')
+    }
+})
